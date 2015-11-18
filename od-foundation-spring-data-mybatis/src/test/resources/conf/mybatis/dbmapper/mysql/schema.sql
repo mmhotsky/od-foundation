61e9de5a2735884d80cc0b86dc8abff257826917
情天 <hotxubin@gmail.com>
2015-11-18 13:06:49 +0800

    fixed errors

--- od-foundation-spring-data-mybatis/src/test/resources/conf/mybatis/dbmapper/mysql/schema.sql
+++ od-foundation-spring-data-mybatis/src/test/resources/conf/mybatis/dbmapper/mysql/schema.sql
@@ -1,7 +1,3 @@
 
 DROP TABLE IF EXISTS od_foundation_db_test;
-<<<<<<< HEAD
 CREATE TABLE IF NOT EXISTS od_foundation_db_test (id INT primary key not null auto_increment, str VARCHAR(100));
-=======
-CREATE TABLE IF NOT EXISTS od_foundation_db_test (id INT, str VARCHAR(100));
->>>>>>> branch 'master' of https://git.oschina.net/mmhotsky/od-foundation.git
